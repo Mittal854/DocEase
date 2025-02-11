558ac3f788610eac3f692caadacec5d4ba1f849a
Mittal854 <bhuveshmittal854@gmail.com>
2025-02-11 20:41:47 +0530

    Move server logic from server.js to api/index.js for better organization

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,11 +1,11 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongodb.js'
-import connectCloudinary from './config/cloudinary.js'
-import adminRouter from './routes/adminRoute.js'
-import doctorRouter from './routes/doctorRoute.js'
-import userRouter from './routes/userRoute.js'
+import connectDB from '../config/mongodb.js'
+import connectCloudinary from '../config/cloudinary.js'
+import adminRouter from '../routes/adminRoute.js'
+import doctorRouter from '../routes/doctorRoute.js'
+import userRouter from '../routes/userRoute.js'
 
 const app=express()
 const port=process.env.PORT || 4000
